88a83485580ad237dbe6f42658b19d306a00f0cf
Ayushiiism <ayushtripathy08@gmail.com>
2023-10-12 12:19:39 +0530

    initial commit

---
+++ src/main/java/com/example/Swift_backend/SwiftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Swift_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Swift_backend/SwiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Swift_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
