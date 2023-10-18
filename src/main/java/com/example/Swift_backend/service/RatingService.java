543fd98b6af50b7a3afb99b1b591658e21c080f1
Daksh Bhatia <dakshbh@amdocs.com>
2023-10-18 11:59:45 +0530

    add rating api

--- src/main/java/com/example/Swift_backend/service/RatingService.java
+++ src/main/java/com/example/Swift_backend/service/RatingService.java
@@ -4,4 +4,6 @@ import com.example.Swift_backend.model.Rating;
 
 public interface RatingService {
     Rating updateConsolidatedRating(Rating rating);
+
+    Rating addNewRating(long rating_id, int userRating);
 }
